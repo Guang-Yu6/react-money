c7847016233bd61927260c83a9a17d3d34eb91bd
Guang-Yu6 <everyting-day@foxmail.com>
2021-04-23 21:33:42 +0800

    路由和导航栏布局搞定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,96 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  //border:1px solid red;
+  height: 100vh;
+  display: flex;  // 默认左右结构
+  flex-direction: column;  // 变成上下结构
+`;
+const Main = styled.div`
+  //border: 1px solid aquamarine;
+  flex-grow: 1;  // 让他尽量的高
+  overflow: auto; //超出后滚动条
+`;
+const Nav = styled.nav`
+  border: 10px solid blue;
+  > ul {
+      display: flex;
+    > li{
+      width: 33.333333%;  // 分开他们3个标签
+      text-align: center;   // 字居中
+      padding: 16px;
+    }
+  }
+  
+`
 
 function App() {
   return (
-   <div>你好</div>
+    <Router>
+      <Wrapper>
+        <Main><Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+
+          <Route path="/money">
+            <Money/>
+          </Route>
+
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+
+          <Redirect exact from='/' to="/money"/>
+
+          <Route path="*">
+            <NoMatch />
+          </Route>
+
+        </Switch></Main>
+
+        <Nav>
+          <ul>
+            <li>
+              <Link to="tags">标签页</Link>
+            </li>
+            <li>
+              <Link to="/money">记账页</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计页</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
+}
+
+function NoMatch(){
+  return (
+    <div>页面不存在</div>
+  )
+}
+
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
 }
 
 export default App;
